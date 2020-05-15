9e92bfef68d75bcbfbddfc53d70cc56967dba663
Juan <j.d.delacruz120@gmail.com>
2020-05-14 21:21:42 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import{
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+
+//pages components
+import HomeProjects from './pages/HomeProjects.js';
+import Contact from './pages/contact.js';
+import About from './pages/about.js';
+import NotFound from './pages/404.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <HomeProjects/>
+        </Route>
+        <Route path='/contact'>
+          <Contact/>
+        </Route>
+        <Route path='/about'>
+          <About/>
+        </Route>
+        <Route path='*'>
+          <NotFound/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/HomeProjects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HomeProjects extends React.Component{
+    render(){
+        return(
+            <h1>Home</h1>
+        );
+    }
+}
+
+
+export default HomeProjects;
