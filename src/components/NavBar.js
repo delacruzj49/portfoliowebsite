419954296aed6b9bf96975cd3d1f4404422be2cb
Juan De La Cruz <juandelacruz@Juans-Mac-mini.local>
2021-06-24 22:00:47 -0500

    imported react-bootstrap components for the Nav bar. Also renamed the component.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-class NavBar extends React.Component{
+//import bootstrap components
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
+class NavigationBar extends React.Component{
     render(){
         return(
             <React.Fragment>
-            
+                <Navbar expand="lg">
+                    <Navbar.Brand href="#home">Personal-Page</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Project</Nav.Link>
+                        <Nav.Link href="#link">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             </React.Fragment>
         );
     }
 }
 
-export default NavBar;
+export default NavigationBar;
