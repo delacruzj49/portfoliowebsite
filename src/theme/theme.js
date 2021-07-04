6c54c567b07914f81c2b161e7dbb6b60563db0ea
Juan De La Cruz <juandelacruz@Juans-Mac-mini.local>
2021-07-04 18:20:13 -0500

    Main colors and Text Sizes for the Website

---
+++ src/theme/theme.js
@@ -0,0 +1,20 @@
+const Theme =  {
+    colorPrimary:"#d8a48f",
+    colorAccent:"#efebce",
+    colorWhite:"white",
+    colorText:"#565360",
+    colorLabel:"#908E9B",
+    colorDisable:"#E1DFE9",
+    lengthSm1:"0.25rem",
+    lengthSm2:"0.375rem",
+    lengthSm3:"0.5rem",
+    lengthMd1:"1rem",
+    lengthMd2:"1.25rem",
+    lengthMd3:"1.5rem",
+    lengthLg1:"2rem",
+    lengthLg2:"3rem",
+    lengthLg3:"4rem"
+
+};
+
+export default Theme;
