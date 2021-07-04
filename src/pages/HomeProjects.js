a85a09d28af206bc1979437ddf0eec82bf359070
Juan De La Cruz <juandelacruz@Juans-Mac-mini.local>
2021-07-04 18:22:00 -0500

    removed typewritting animation because its lame

--- src/pages/HomeProjects.js
+++ src/pages/HomeProjects.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Styled, { keyframes } from 'styled-components';
-
+import Styled from 'styled-components';
+import Theme from '../theme/theme.js';
 //Import components
 import Navbar from '../components/NavBar.js';
 
@@ -8,31 +8,8 @@ const Page = Styled.div`
 margin: auto;
 width: auto;
 height: 2000px;
-background-color: black;
+background-color: ${Theme.colorGray};
 text-align:center;
-`
-
-const Typing = keyframes`
-from { width: 0 }
-  to { width: 100% }
-`;
-
-const Blink = keyframes`
-  from, to { border-color: transparent }
-  50% { border-color: green; }
-`;
-
-
-const Welcome = Styled.h1`
-overflow: hidden;
-border-right: .15em solid green; 
-white-space: nowrap;
-margin: 0 auto; 
-letter-spacing: .15em; 
-color:#33FF00;
-animation: 
-    ${Typing} 3.5s steps(40, end),
-    ${Blink} .60s step-end infinite;
 `
 
 class HomeProjects extends React.Component{
@@ -41,7 +18,7 @@ class HomeProjects extends React.Component{
             <React.Fragment>
                 <Navbar/>
                 <Page>
-                    <Welcome>Hello Welcome To My Page</Welcome>
+                    
                 </Page>
             </React.Fragment>
         );
