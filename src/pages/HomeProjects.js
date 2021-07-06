b8e0c04189023f3e1548a9893dc1953faf064fa9
Juan De La Cruz <juandelacruz@Juans-Mac-mini.local>
2021-07-05 20:12:22 -0500

    imported bootstrap components. Removed styled component created. Moved Styled Components to the bottom.

--- src/pages/HomeProjects.js
+++ src/pages/HomeProjects.js
@@ -3,14 +3,10 @@ import Styled from 'styled-components';
 import Theme from '../theme/theme.js';
 //Import components
 import Navbar from '../components/NavBar.js';
+//import bootstrap
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
-const Page = Styled.div`
-margin: auto;
-width: auto;
-height: 2000px;
-background-color: ${Theme.colorGray};
-text-align:center;
-`
+
 
 class HomeProjects extends React.Component{
     render(){
@@ -18,6 +14,7 @@ class HomeProjects extends React.Component{
             <React.Fragment>
                 <Navbar/>
                 <Page>
+                    <Hero fluid>  </Hero>
                     
                 </Page>
             </React.Fragment>
@@ -27,3 +24,16 @@ class HomeProjects extends React.Component{
 
 
 export default HomeProjects;
+
+
+const Page = Styled.div`
+margin: auto;
+width: auto;
+height: 2000px;
+background-color: ${Theme.colorGray};
+text-align:center;
+`
+const Hero = Styled(Jumbotron)`
+background-color:${Theme.colorwhite};
+background-image: -webkit-linear-gradient(30deg, ${Theme.colorPrimary} 50%, ${Theme.colorAccent} 50%);
+`
