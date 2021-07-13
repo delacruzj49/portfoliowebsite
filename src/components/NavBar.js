bc04ee1122856815a052c098f1108715c292da59
Juan De La Cruz <juandelacruz@Juans-Mac-mini.local>
2021-07-12 21:08:51 -0500

    For this version i changed the orientation and location of the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Styled from 'styled-components';
+
+
 
 class NavBar extends React.Component{
     render(){
         return(
             <React.Fragment>
-            
+                
+                <MainNav defaultActiveKey="/home" className="flex-column">
+                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link eventKey="link-1">About</Nav.Link>
+                    <Nav.Link eventKey="link-2">Projects</Nav.Link>
+                </MainNav>
             </React.Fragment>
         );
     }
 }
 
 export default NavBar;
+
+
+const MainNav = Styled(Nav)`
+background-color:black;
+width:25%;
+position:left;
+height:2000px;
+`
