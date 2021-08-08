e09abb6a0d99cc53e41243c397e2eca1fe9837b7
Juan De La Cruz <juandelacruz@Juans-Mac-mini.local>
2021-08-08 16:16:51 -0500

    Styling for main project

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+const Theme =  {
+    colorPrimary:"#1E6091",
+    colorAccent:"#1A759F",
+    colorWhite:"white",
+    colorText:"#565360",
+    colorLabel:"#908E9B",
+    colorDisable:"#E1DFE9",
+    lengthSm1:"0.25rem",
+    lengthSm2:"0.375rem",
+    lengthSm3:"0.5rem",
+    lengthMd1:"1rem",
+    lengthMd2:"1.25rem",
+    lengthMd3:"1.5rem",
+    lengthLg1:"2rem",
+    lengthLg2:"3rem",
+    lengthLg3:"4rem"
+};
+
+export default Theme;
