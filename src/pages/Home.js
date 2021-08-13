7e24c57c02cfe3b50344d3ad5d26a7847d6d1e22
Juan De La Cruz <Juan De La Cruz>
2021-08-13 11:53:58 -0500

    1. created component to display welcome 2. Added welcome message and styling 3. Added Link to point to resume

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,52 @@
 import React from 'react';
+import Styled from 'styled-components';
+import Theme from '../theme.js';
+import Image from '../assets/welcome.jpg';
+import {BrowserRouter as Router } from 'react-router-dom'; 
 
-class HomeProjects extends React.Component{
+class Home extends React.Component{
     render(){
         return(
-            <React.Fragment>
-                <h1>Hello World</h1>
-            </React.Fragment>
+            <Router>
+                <Page>
+                    <Welcome>Welcome!</Welcome>
+                    <About>My Name Is Juan, And I Am A Web Developer Located In Houston,Texas</About>
+                    <Resume href="/about">Learn More About Me</Resume>
+                </Page>
+            </Router>
         );
     }
 }
 
 
-export default HomeProjects;
+export default Home;
+
+const Page = Styled.div`
+background-image:url(${Image});
+background-size:cover;
+height:78vh;
+width:auto;
+text-align:center;
+overflow:hidden;
+border-radius:10px;
+`
+
+const Welcome = Styled.h1`
+margin-top:25vh;
+color: ${Theme.colorWhite};
+font-size:${Theme.lengthLg3};
+`
+
+const About = Styled.h2`
+color: ${Theme.colorWhite};
+font-size:${Theme.lengthMd3};
+`
+const Resume= Styled.a`
+border:none;
+background:none;
+font-size:${Theme.lengthMd2};
+color:${Theme.colorAccent};
+&:hover{
+    color:${Theme.colorWhite};
+}
+`
